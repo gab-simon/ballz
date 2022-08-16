f430d129cec42895b8465a1dc2e5d78369b0df01
gabrielsimonklupp <gabriel.simon@klupp.tech>
2022-08-15 23:20:01 -0300

    blocks daoras

--- game.c
+++ game.c
@@ -23,16 +23,18 @@
 #define BOUNCER_SIZE 15
 #define GRID 15.0
 
+int out = -500;
+
 void must_init(bool test, const char *description)
 {
-    if(test) return;
+    if (test)
+        return;
 
     printf("couldn't initialize %s\n", description);
     exit(1);
 }
 
-
-//randomização
+// randomização
 int between(int lo, int hi)
 {
     return lo + (rand() % (hi - lo));
@@ -40,18 +42,18 @@ int between(int lo, int hi)
 
 typedef struct SPRITES
 {
-    ALLEGRO_BITMAP* _sheet;
+    ALLEGRO_BITMAP *_sheet;
 
-    ALLEGRO_BITMAP* ball;
-    ALLEGRO_BITMAP* life;
+    ALLEGRO_BITMAP *ball;
+    ALLEGRO_BITMAP *life;
 
-    ALLEGRO_BITMAP* block[2];
+    ALLEGRO_BITMAP *block[2];
 } SPRITES;
 SPRITES sprites;
 
-ALLEGRO_BITMAP* sprite_grab(int x, int y, int w, int h)
+ALLEGRO_BITMAP *sprite_grab(int x, int y, int w, int h)
 {
-    ALLEGRO_BITMAP* sprite = al_create_sub_bitmap(sprites._sheet, x, y, w, h);
+    ALLEGRO_BITMAP *sprite = al_create_sub_bitmap(sprites._sheet, x, y, w, h);
     must_init(sprite, "sprite grab");
     return sprite;
 }
@@ -61,11 +63,11 @@ void sprites_init()
     sprites._sheet = al_load_bitmap("utils/spritesheet.png");
     must_init(sprites._sheet, "spritesheet");
 
-    sprites.ball = sprite_grab(0, 0, 50, 60);
+    sprites.ball = sprite_grab(1, 4, 16, 16);
 
     sprites.life = sprite_grab(0, 14, 6, 6);
 
-    sprites.block[0] = sprite_grab(0, 16, 32, 32);
+    sprites.block[0] = sprite_grab(1, 5, 16, 16);
     sprites.block[1] = sprite_grab(8, 18, 23, 55);
 }
 
@@ -81,31 +83,52 @@ void sprites_deinit()
     al_destroy_bitmap(sprites._sheet);
 }
 
-//Blocks
-struct obj{int wx, wy, x, y, w, h;};
+// Blocks
 struct obj
-    block[10][20],
+{
+    int wx, wy, x, y, w, h;
+};
+struct obj
+    block[10][17],
     life = {},
-    bola = {}
-;
+    ball = {};
 
-void blocks(){
+int colidir(int Ax, int Ay, int Aw, int Ah, int Bx, int By, int Bw, int Bh)
+{
+    if ((Ax + Aw >= Bx && Ax <= Bx + Bw && Ay + Ah >= By && Ay <= By + Bh) || (Ax + Aw >= Bx && Ax <= Bx + Bw && !Ay && !Ah && !By && !Bh))
+        return 1;
+    return 0;
+}
+
+void blocks()
+{
     int i, j, draw;
 
-    for (i = 0; i < 10; i++){
-        draw = 16;
-        for (j = 0; j < 20; j++){
-            // if(block[i][j].x != -500){
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 17; j++)
+        {
+            if (block[i][j].x != out)
+            {
+                draw = between(0, 2);
                 al_draw_bitmap(sprites.block[0], block[i][j].wx + (j * 32), block[i][j].wy + (i * 32), 0);
-            // }
+                if (colidir(block[i][j].x, block[i][j].y, block[i][j].w, block[i][j].h, ball.x, ball.y, ball.h, ball.w))
+                {
+                    // if (bouncer_x + bouncer_y <= block[i][j].x || bouncer_x + 1 >= block[i][j].x + block[i][j].w)
+                    //     velx = -velx;
+                    // else
+                    //     vely = -vely;
+                    block[i][j].x = out;
+                    // pontos += 10;
+                }
+            }
         }
-        draw += 32;
     }
 }
 
 void mostra_jogo()
 {
-    ALLEGRO_BITMAP *ball = al_load_bitmap("utils/ball.png");
+    // ALLEGRO_BITMAP *ball = al_load_bitmap("utils/ball.png");
     ALLEGRO_BITMAP *spritess = al_load_bitmap("utils/blocks.png");
     ALLEGRO_BITMAP *cursor = al_load_bitmap("utils/cursor.png");
     ALLEGRO_BITMAP *background = al_load_bitmap("utils/bg.jpg");
@@ -132,8 +155,8 @@ void mostra_jogo()
         bouncer_dx = -GRID,
         bouncer_dy = GRID;
 
-    bouncer_x = RES_WIDTH / 3.0 - BOUNCER_SIZE / 2.0;
-    bouncer_y = RES_HEIGHT / 3.0 - BOUNCER_SIZE / 2.0;
+    ball.x = RES_WIDTH / 3.0 - BOUNCER_SIZE / 2.0;
+    ball.y = RES_HEIGHT / 3.0 - BOUNCER_SIZE / 2.0;
 
     int pontuacao = 0;
 
@@ -167,14 +190,14 @@ void mostra_jogo()
 
                 if (estado)
                 {
-                    if (bouncer_x < 0 || bouncer_x > RES_WIDTH - 5)
+                    if (ball.x < 0 || ball.x > RES_WIDTH - 5)
                         bouncer_dx = -bouncer_dx;
 
-                    if (bouncer_y < 0 || bouncer_y > RES_HEIGHT - 5)
+                    if (ball.y < 0 || ball.y > RES_HEIGHT - 5)
                         bouncer_dy = -bouncer_dy;
 
-                    bouncer_x += bouncer_dx;
-                    bouncer_y += bouncer_dy;
+                    ball.x += bouncer_dx;
+                    ball.y += bouncer_dy;
                     if (game_over == false)
                     {
                         // al_play_sample(ponto, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL);
@@ -199,8 +222,8 @@ void mostra_jogo()
                 break;
 
             case ALLEGRO_EVENT_MOUSE_AXES:
-                bouncer_x = evento.mouse.x;
-                bouncer_y = evento.mouse.y;
+                ball.x = evento.mouse.x;
+                ball.y = evento.mouse.y;
                 break;
             }
 
@@ -217,7 +240,7 @@ void mostra_jogo()
             al_draw_bitmap(background, 0, 0, 0);
             blocks();
             al_draw_textf(fonte, al_map_rgb(0xFF, 0xFF, 0xFF), 10, 10, ALLEGRO_ALIGN_LEFT, "%i", pontuacao);
-            al_draw_bitmap(ball, bouncer_x, bouncer_y, 0);
+            al_draw_bitmap(sprites.ball, ball.x, ball.y, 0);
             al_flip_display();
         }
 
@@ -255,7 +278,7 @@ void mostra_jogo()
     al_destroy_sample(ponto);
     al_destroy_sample(batida);
     al_destroy_sample(som_pulo);
-    al_destroy_bitmap(ball);
+    // al_destroy_bitmap(ball);
     al_destroy_bitmap(spritess);
     al_destroy_bitmap(background);
     al_destroy_event_queue(fila_eventos);
